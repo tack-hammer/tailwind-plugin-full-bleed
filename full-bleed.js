9b801114b397d5d458c2f5d11bd74f106e3e90ad
Thierry Wood <thierry@onemore.group>
2019-07-27 09:27:55 +0700

    init

---
+++ full-bleed.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+  return function({ addUtilities, variants }) {
+    addUtilities(
+      {
+        'full-bleed': {
+          width: '100vw',
+          'margin-inline-start': '50%',
+          'margin-inline-end': 'unset',
+          transform: 'translateX(-50%)',
+          'max-width': 'none',
+        },
+      },
+      variants('full-bleed')
+    )
+  }
+}
